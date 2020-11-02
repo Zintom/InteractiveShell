75107228db4f823318e7a24b07bccfbd784502e4
Zintom <tzsinkoapps@gmail.com>
2020-11-02 19:34:18 +0000

    Added DemoProject

---
+++ ZintomShellHelper/DemoProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZintomShellHelper;
+
+namespace DemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int menuResult = MenuManager.CreateMenu(new string[] { "Option 1", "Option 2", "Option 3", "Option 4", "Option 5" }, false, 2);
+
+        }
+    }
+}
