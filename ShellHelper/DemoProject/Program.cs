ddc92fae5dd6e56e01ae5a9d974dd2c21df70516
Zintom <tzsinkoapps@gmail.com>
2020-11-13 16:25:14 +0000

    Demo adapted to work with 2.0

--- ShellHelper/DemoProject/Program.cs
+++ ShellHelper/DemoProject/Program.cs
@@ -7,9 +7,25 @@ namespace DemoProject
     {
         static void Main(string[] args)
         {
-            ShellMenu.Init();
-            ShellMenu.DrawTitle("Title", "Subtitle", "Content", true);
-            int menuResult = ShellMenu.CreateMenu(new string[] { "Option 1", "Option 2", "Option 3", "Option 4", "Option 5" }, false, 2);
+            ShellMenu menu = new ShellMenu();
+
+            ShellDisplayOptions displayOptions = new ShellDisplayOptions()
+            {
+                LeftOffset = 2,
+                DisplayHorizontally = false
+            };
+
+            ShellTitleDisplayOptions titleDisplayOptions = new ShellTitleDisplayOptions()
+            {
+                LeftOffset = 2
+            };
+
+            while (true)
+            {
+                menu.DrawTitle("Hi!", titleDisplayOptions);
+                menu.DrawTitle("Title", "Hello\nLol\nSubtitle Perferendis laboriosam commodi occaecati ipsam", "Content Magnam ratione aut dolor exercitationem", titleDisplayOptions, false);
+                int menuResult = menu.DisplayMenu(new string[] { "Option 1", "Option 2", "Option 3", "Option 4", "Option 5" }, displayOptions);
+            }
         }
     }
 }
