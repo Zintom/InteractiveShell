1aa75fcc8952cb45371f7dce0a0a9f2e0be325d8
Zintom <tzsinkoapps@gmail.com>
2020-11-23 12:17:10 +0000

    Updated Demo

--- InteractiveShell/DemoProject/Program.cs
+++ InteractiveShell/DemoProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using Zintom.InteractiveShell;
+﻿using System;
+using Zintom.InteractiveShell;
 
 namespace DemoProject
 {
@@ -12,15 +13,20 @@ namespace DemoProject
             // Create the options for theming.
             ShellDisplayOptions displayOptions = new ShellDisplayOptions()
             {
-                LeftOffset = 2
+                LeftOffset = 2,
+                DisplayHorizontally = false
             };
             ShellTitleDisplayOptions titleDisplayOptions = new ShellTitleDisplayOptions()
             {
                 LeftOffset = 2
             };
 
+            //
+            // Single choice menu
+            //
+
             // Draw the title, clearing the screen beforehand
-            menu.DrawTitle(title: "Sample Menu", subtitle: "Select an option:", displayOptions: titleDisplayOptions, clearScreen: true);
+            menu.DrawTitle(title: "Sample Menu", subtitle: "Select a single option:", displayOptions: titleDisplayOptions, clearScreen: true);
 
             // Present the user with the interactive menu
             int result = menu.DisplayMenu(options: new string[] { "Option 1", "Option 2" }, displayOptions: displayOptions);
@@ -33,6 +39,25 @@ namespace DemoProject
                     // Do Option 2
                     break;
             }
+
+            //
+            // Multi-choice menu
+            //
+
+            // Draw the title, clearing the screen beforehand
+            menu.DrawTitle(title: "Sample Menu", subtitle: "Select multiple options with Control + Enter:", displayOptions: titleDisplayOptions, clearScreen: true);
+
+            // Present the user with the interactive menu
+            int[] results = menu.DisplayMultiMenu(options: new string[] { "Option 1", "Option 2", "Option 3" }, displayOptions: displayOptions);
+
+            Console.WriteLine("Selected indexes:");
+            foreach (var index in results)
+            {
+                Console.WriteLine(index);
+            }
+
+            menu.Reset();
+            Console.ReadKey();
         }
     }
 }
