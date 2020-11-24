8285708f49b7860365d78f1651eb06e45135a6db
Zintom <tzsinkoapps@gmail.com>
2020-11-24 14:12:30 +0000

    Update Program.cs

--- InteractiveShell/DemoProject/Program.cs
+++ InteractiveShell/DemoProject/Program.cs
@@ -21,15 +21,18 @@ namespace DemoProject
                 LeftOffset = 2
             };
 
+            menu.FallbackDisplayOptions = displayOptions;
+            menu.FallbackTitleDisplayOptions = titleDisplayOptions;
+
             //
             // Single choice menu
             //
 
             // Draw the title, clearing the screen beforehand
-            menu.DrawTitle(title: "Sample Menu", subtitle: "Select a single option:", displayOptions: titleDisplayOptions, clearScreen: true);
+            menu.DrawTitle(title: "Sample Menu", subtitle: "Select a single option:", "Content test", displayOptions: null, clearScreen: true);
 
             // Present the user with the interactive menu
-            int result = menu.DisplayMenu(options: new string[] { "Option 1", "Option 2" }, displayOptions: displayOptions);
+            int result = menu.DisplayMenu(options: new string[] { "Option 1", "Option 2" }, displayOptions: null);
             switch (result)
             {
                 case 0:
@@ -45,15 +48,15 @@ namespace DemoProject
             //
 
             // Draw the title, clearing the screen beforehand
-            menu.DrawTitle(title: "Sample Menu", subtitle: "Select multiple options with Control + Enter:", displayOptions: titleDisplayOptions, clearScreen: true);
+            menu.DrawTitle(title: "Sample Menu", subtitle: "Select multiple options with Control + Enter:", displayOptions: null, clearScreen: true);
 
             // Present the user with the interactive menu
-            int[] results = menu.DisplayMultiMenu(options: new string[] { "Option 1", "Option 2", "Option 3" }, displayOptions: displayOptions);
+            int[] results = menu.DisplayMultiMenu(options: new string[] { "Option 1", "Option 2", "Option 3" }, displayOptions: null);
 
-            Console.WriteLine("Selected indexes:");
+            menu.DrawContentText("\nSelected indexes:", false);
             foreach (var index in results)
             {
-                Console.WriteLine(index);
+                menu.DrawContentText(index.ToString(), false);
             }
 
             menu.Reset();
